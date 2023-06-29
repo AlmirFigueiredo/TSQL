f8c63afcfa1d9fe1bd0f769920df28c0d0a9978f
Jose Almir <joseamfj@outlook.com>
2023-06-29 18:11:23 -0300

    insert into first table

---
+++ First_Table/insert_values.sql
@@ -0,0 +1,10 @@
+INSERT INTO EmployeeDemographics VALUES
+(1001, 'Jim', 'Helpert', 30, 'Male'),
+(1002, 'Marcos', 'Cesar', 98, 'Male' ),
+(1003, 'Antonio', 'Carlos', 29, 'Male'),
+(1004, 'Maria', 'Luiza', 24, 'Female'),
+(1005, 'Julia', 'Silva', 20, 'Female'),
+(1006, 'Albert', 'Santos', 17, 'Male'),
+(1007, 'Milena', 'Castro', 28, 'Female'),
+(1008, 'Antonieta', 'Carvalho', 57, 'Female'),
+(1009, 'Mateus', 'Lins', 48, 'Male');
