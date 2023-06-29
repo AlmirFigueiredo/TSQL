499e5644a8f7775f779ddc05cd4def3060eefe3f
Jose Almir <joseamfj@outlook.com>
2023-06-29 18:20:39 -0300

    Insert employees salaries

---
+++ First_Tables/Employee Salaries/insert_values.sql
@@ -0,0 +1,10 @@
+Insert Into EmployeeSalary VALUES
+(1001, 'HelpDesk', 50000),
+(1002, 'Software Engineer', 100000),
+(1003, 'Cloud Architect', 140000),
+(1004, 'Data Analyst', 80000),
+(1005, 'Data Engineer', 120000),
+(1006, 'HR Manager', 180000),
+(1007, 'Sales Manager', 200000),
+(1008, 'Softwar Engineer', 115000),
+(1009, 'Data Analyst', 95000);
